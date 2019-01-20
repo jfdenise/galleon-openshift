3beeccc5c3a194b137a88b6f7a5eb78d24f257ea
Jean-Francois Denise <jfdenise@gmail.com>
2019-01-20 17:04:05 +0100

    script run

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,4 +4,7 @@ mvn package -Dinstall.dir=/wildfly-core -DwildflyLocation=$WILDFLY_LOCATION
 rm -rf ~/.m2
 chmod -R 777 /wildfly-core
 
-
+git clone $GIT_REPOSITORY /git-src
+cd /git-src
+mvn clean install -Popenshift
+cp /git-src/target/ROOT.war /wildfly-core/standalone/deployments/ROOT.war
