36eb2c1d58c5523ffd3d0269c6e630245b39adc6
Jean-Francois Denise <jfdenise@gmail.com>
2019-01-20 17:41:29 +0100

    build script

--- scripts/build-src.sh
+++ scripts/build-src.sh
@@ -3,8 +3,3 @@ git clone $GIT_REPOSITORY /git-src
 cd /git-src
 mvn clean install -Popenshift
 cp /git-src/target/ROOT.war /wildfly-core/standalone/deployments/ROOT.war
-
-git clone $GIT_REPOSITORY /git-src
-cd /git-src
-mvn clean install -Popenshift
-cp /git-src/target/ROOT.war /wildfly-core/standalone/deployments/ROOT.war
